8611d86a746527dd01b7ceb609d767b67bc40538
Marcus Chang <30785471+Rynhalt@users.noreply.github.com>
2024-09-17 15:18:37 -0400

    Revert "optional = false"
    This reverts commit 185ffe0e64bb532e7c026c4d8ef3eb99a60879f0.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8b607617503aba06"; }
+/******/ 		__webpack_require__.h = function() { return "2d230c8bdaade379"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
